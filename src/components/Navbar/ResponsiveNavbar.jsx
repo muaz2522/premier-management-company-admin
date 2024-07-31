381e09a9ae7f002a59731a75f3811def566270e8
unknown <oyemauz@gmail.com>
2024-07-31 17:01:45 +0500

    modified

--- src/components/Navbar/ResponsiveNavbar.jsx
+++ src/components/Navbar/ResponsiveNavbar.jsx
@@ -6,7 +6,7 @@ export default function ResponsiveNavbar({ isOpen, setIsOpen }) {
     const toggle = () => setIsOpen(!isOpen);
     return (
         <div>
-            <Offcanvas className="w-[50%]" zIndex={500} direction={"end"} isOpen={isOpen} onClick={toggle}>
+            <Offcanvas className="custom-min:w-[40%] custom-md:w-[50%]" zIndex={500} direction={"end"} isOpen={isOpen} onClick={toggle}>
                 <OffcanvasHeader toggle={toggle}>
                     {/* Offcanvas */}
                 </OffcanvasHeader>
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Navbar from "../components/Navbar/NavBar";
-import ResponsiveNavbar from "../components/Responsive/ResponsiveNavbar";
+import ResponsiveNavbar from "../components/Navbar/ResponsiveNavbar";
+
 
 function Header() {
     const [isOpen, setIsOpen] = useState(false);
