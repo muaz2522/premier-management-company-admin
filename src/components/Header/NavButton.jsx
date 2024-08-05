9ebdb8a8085174a392a6fd890ccb489972ffba17
unknown <oyemauz@gmail.com>
2024-08-05 15:34:43 +0500

    contact changed

--- src/components/Header/NavButton.jsx
+++ src/components/Header/NavButton.jsx
@@ -8,7 +8,7 @@ function NavButton() {
                 <NavLink className={`hover:text-stone-700 text-text-color sm:py-2 sm:px-4 custom-min:border custom-min:py-2 custom-min:px-8 custom-min:rounded-3xl sm:rounded-none sm:w-24 sm:border-0 sm:border-r `} to="/login">Login</NavLink>
                 <NavLink className={`hover:text-stone-700 text-text-color sm:py-2 sm:px-4 custom-min:w-auto custom-min:border custom-min:py-2 custom-min:px-8 custom-min:rounded-3xl sm:rounded-none sm:border-0  `} to="/signUp">Sign Up</NavLink>
             </button>
-            <ButtonLink link="/contact" text="Get in touch" className="py-2 px-4" />
+            <ButtonLink link="/contact" text="Get in touch" className="py-2 px-4 hover:text-stone-700 text-text-color" />
         </div>
     )
 }
