fb64574fcbb920a0ce948078c07449bb4a7c9902
unknown <oyemauz@gmail.com>
2024-08-06 23:24:15 +0500

    link modify

--- src/dashboard/components/home/NavPost.jsx
+++ src/dashboard/components/home/NavPost.jsx
@@ -2,7 +2,7 @@ function NavPost({ className }) {
     return (
         <div className={`hidden custom-min:hidden md:block`} style={{ display: className }}>
             <div className="custom-min:ml-4 md:ml-0 md:bg-ground md:h-[500px] p-4 flex flex-col gap-3">
-                <span className="text-text-color">All Posts</span>
+                <span className="text-black">All Posts</span>
                 <span className="text-text-color">Recent Posts</span>
                 <span className="text-text-color">Articles</span>
                 <span className="text-text-color">Photo Based</span>
