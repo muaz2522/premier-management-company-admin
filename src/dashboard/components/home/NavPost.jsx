c34a2149c61af171f4f3c9dc005ceb17a170571c
unknown <oyemauz@gmail.com>
2024-08-06 19:04:05 +0500

    dashboard changes commit

--- src/dashboard/components/home/NavPost.jsx
+++ src/dashboard/components/home/NavPost.jsx
@@ -1,7 +1,7 @@
 function NavPost({ className }) {
     return (
         <div className={`hidden custom-min:hidden md:block`} style={{ display: className }}>
-            <div className="custom-min:ml-4 md:ml-0 md:bg-ground md:h-[500px] p-4 flex flex-col gap-4">
+            <div className="custom-min:ml-4 md:ml-0 md:bg-ground md:h-[500px] p-4 flex flex-col gap-1">
                 <p>All Posts</p>
                 <p>Recent Posts</p>
                 <p>Articles</p>
