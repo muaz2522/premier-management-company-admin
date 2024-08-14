8930bbc1fa4e09752263121baccc6f4540cda967
unknown <oyemauz@gmail.com>
2024-08-14 10:07:30 +0500

    admin panel commit

--- src/admin-dashboard/components/users/index.jsx
+++ src/admin-dashboard/components/users/index.jsx
@@ -8,7 +8,7 @@ function User({user}) {
                         <span className="text-3xl font-bold">All Users</span>
                     </div>
                     <div className="text-lg py-8">
-                        <header className="grid grid-cols-[1.5fr_1.5fr_2.5fr_1.2fr_1.2fr_1fr] p-4 border">
+                        <header className="grid grid-cols-[1.5fr_1.5fr_2.5fr_1.2fr_1.2fr_1.2fr] p-4 border">
                             <div className="font-semibold">image</div>
                             <div className="font-semibold">name</div>
                             <div className="font-semibold">email</div>
--- src/admin-dashboard/components/users/userList.jsx
+++ src/admin-dashboard/components/users/userList.jsx
@@ -8,13 +8,13 @@ function UserList({ user }) {
     console.log(isModal)
     return (
         <>
-            <div className="grid grid-cols-[1.5fr_1.5fr_2.5fr_1.2fr_1.2fr_1fr] bg-white px-2 py-3 border">
+            <div className="grid grid-cols-[1.5fr_1.5fr_2.5fr_1.2fr_1.2fr_1.2fr] bg-white px-2 py-3 border">
                 <img src={user.imageUrl ? user.imageUrl : src} alt="user" className="w-12" />
                 <div>{user.firstName}</div>
                 <div>{user.email}</div>
                 <div>{user.jobTitle}</div>
                 <div>{user.experience}</div>
-                <div onClick={()=>setIsModal(!isModal)}><Button className="px-4 py-2">View User</Button></div>
+                <div onClick={()=>setIsModal(!isModal)}><Button className="px-4 py-2">View Detail</Button></div>
             </div>
             {isModal ? <UserModal user={user} setIsModal={setIsModal} /> : ""}
         </>
